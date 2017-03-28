849accc433a02905bfdae98fbebd7945ac1ee4d6
Ken Moussat <ken.moussat@devinci.fr>
2017-03-28 22:33:20 +0200

    Initial Commit
    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+   console.log('Hellooooooooooooooooo!')
+   
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  
+  
+})
+
+app.post('/quotes', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  console.log(req.body)
+  
+})
