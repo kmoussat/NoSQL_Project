04fa6ca72b3f5f46441769d7e56c574d3e3510d7
papuech <paul.puech@devinci.fr>
2017-03-31 21:02:51 +0200

    Initialisation db + bouton ajout de quote

--- readme.txt
+++ readme.txt
@@ -1,1 +1,7 @@
 https://zellwk.com/blog/crud-express-mongodb/
+
+Base de données mongodb :
+Database username : regular
+Database password : regularesilv1
+adresse pour connection : mongodb://regular:regularesilv1@ds145750.mlab.com:45750/nosql_project
+	
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+var db
 
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}))
 
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-   console.log('Hellooooooooooooooooo!')
-   
+MongoClient.connect('mongodb://regular:regularesilv1@ds145750.mlab.com:45750/nosql_project', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000 and connected to mongodb')
+  })
 })
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
-  
-  
 })
 
 app.post('/quotes', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  console.log(req.body)
-  
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
+
+app.get('/getquotes', (req, res) => {
+  var cursor = db.collection('quotes').find().toArray(function(err, results) {
+  console.log(results)
+  // send HTML file populated with quotes here
+})
+
